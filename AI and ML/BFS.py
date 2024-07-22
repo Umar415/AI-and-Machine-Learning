0c22e03a5a0e608735d44e277c9ea1ffb9ee472f
Umar415 <u41514@gmail.com>
2024-07-22 11:19:27 +0500

    AI and ML LABs (MERL Journey)

---
+++ AI and ML/BFS.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def bfs(graph, start_node):
+    visited = set()
+    queue = deque([start_node])
+    visited.add(start_node)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print("BFS traversal starting from node 'A':")
+bfs(graph, 'A')
