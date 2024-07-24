b66c2e157cd5373601427e91430984cc41abe1f1
Muhammad Umar <136614256+Umar415@users.noreply.github.com>
2024-07-24 22:20:43 +0500

    Delete Functional Programming (Scala) directory

--- Functional Programming (Scala)/PrintReverseArray.scala
+++
@@ -1,9 +0,0 @@
-object printreverse {
-	def main(args: Array[String]): Unit = {
-	  var arr = Array(12,32,14,12,43,22)
-	  
-	  for(i <- (arr.length-1 to 0 by -1)) {
-	    println(arr(i))
-	  }
-	}
-}
